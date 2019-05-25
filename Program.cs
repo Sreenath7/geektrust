dcefe4594aa69f911d48404e9a4966e6b9547247
Sreenath7 <sreenathpb9951@gmail.com>
2019-05-25 22:51:02 +0530

    inital code changes family tree

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace geektrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var familyMembers =  new List<FamilyMember>();
+        }
+
+        public void AddChild(List<FamilyMember> familyMembers, string mother,string child,Gender gender)
+        {
+            
+
+        }
+
+        public FamilyMember GetMember(List<FamilyMember> familyMembers,string memberName)
+        {
+            var memberInfo = familyMembers.FirstOrDefault(f=>f.Name==memberName);
+
+            return memberInfo;
+        }
+    }
+}
